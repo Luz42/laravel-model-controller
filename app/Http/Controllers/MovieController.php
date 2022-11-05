4f74bc0d1b0bb1dcb6e5ffe86241c5bbbabb845c
Luz42 <a.evangelista93@gmail.com>
2022-11-05 13:15:00 +0100

    Implementata la gestione di model e view

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
+
 class MovieController extends Controller
 {
     //all'interno del controller verranno abbinati e gestiti i dati e la pagina,
     //per essere poi associati ad una determinata rotta 
-    
+
     public function index(){
-        return view('welcome');
+
+        $movie = Movie::all();
+        //dd($movie);
+        return view('home', compact('movie'));
     }
 }

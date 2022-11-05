6957c6aa45c23f599dfa641cddb12521b858029d
Luz42 <a.evangelista93@gmail.com>
2022-11-05 13:21:26 +0100

    Creato card per la visualizzazion dei dati

---
+++ resources/views/Partials/card.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <ul>
+        <li>Titolo: {{$item['title']}}</li>
+        <li>Titolo originale:{{$item['original_title']}}</li>
+        <li>Origine:{{$item['nationality']}}</li>
+        <li>Uscita:{{$item['date']}}</li>
+        <li>Voto:{{$item['vote']}}</li>
+    </ul>
+</div>
